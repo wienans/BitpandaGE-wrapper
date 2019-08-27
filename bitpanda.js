304bcd200ad186bcc40846c389f15893e9b66ee6
wienans <sven.wienand@outlook.com>
2019-08-27 14:58:45 +0200

    Added Private Funcitons to Wrapper

--- bitpanda.js
+++ bitpanda.js
@@ -22,34 +22,30 @@ var BitpandaError = function BitpandaError(message, meta) {
   this.meta = meta;
 };
 
-var Bitpanda = function (key, secret, client_id, timeout, host) {
+var Bitpanda = function (key, timeout, host) {
   this.key = key;
   this.timeout = timeout || 5000;
   this.host = host || 'api.exchange.bitpanda.com';
-
-  // Unused
-  this.secret = secret;
-  this.client_id = client_id;
-
+  this.private = false;
   _.bindAll(this);
 }
 
-Bitpanda.prototype._request = function (method, path, data, callback, args) {
+Bitpanda.prototype._request = function (method, path, pdata, callback, args) {
   var timeout = this.timeout;
+  data = undefined;
   var options = {
     host: this.host,
     path: path,
-    method: method
-    // headers: {
-    //   'User-Agent': 'Mozilla/4.0 (compatible; Bitstamp node.js client)'
-    // }
+    method: method,
+    headers: {}
   };
-
-  if (method === 'post') {
-    options.headers['Content-Length'] = data.length;
-    options.headers['content-type'] = 'application/x-www-form-urlencoded';
+  if (this.key && this.private) {
+    options.headers['Authorization'] = 'Bearer ' + this.key;
   }
-
+  if (method === 'post') {
+    options.headers['Content-Length'] = pdata.length;
+    options.headers['content-type'] = 'application/json';
+  }
   var req = https.request(options, function (res) {
     res.setEncoding('utf8');
     var buffer = '';
@@ -57,8 +53,8 @@ Bitpanda.prototype._request = function (method, path, data, callback, args) {
       buffer += data;
     });
     res.on('end', function () {
-      if (res.statusCode !== 200) {
-        console.log(buffer);
+      if (res.statusCode !== 200 && res.statusCode !== 204 && res.statusCode !== 201) {
+        // console.log(buffer);
         var message;
 
         try {
@@ -68,6 +64,10 @@ Bitpanda.prototype._request = function (method, path, data, callback, args) {
         }
 
         return callback(new BitpandaError('Bitpanda error ' + res.statusCode, message));
+      }
+      if (res.statusCode === 204) {
+        var json = JSON.parse('{"order_deleted":true}');
+        return callback(json)
       }
       try {
         var json = JSON.parse(buffer);
@@ -90,29 +90,9 @@ Bitpanda.prototype._request = function (method, path, data, callback, args) {
       req.abort();
     });
   });
-
+  if (method === 'post')
+    req.write(pdata);
   req.end(data);
-}
-
-// if you call new Date too fast it will generate
-// the same ms, helper to make sure the nonce is
-// truly unique (supports up to 999 calls per ms).
-Bitpanda.prototype._generateNonce = function () {
-  var now = new Date().getTime();
-
-  if (now !== this.last)
-    this.nonceIncr = -1;
-
-  this.last = now;
-  this.nonceIncr++;
-
-  // add padding to nonce incr
-  // @link https://stackoverflow.com/questions/6823592/numbers-in-the-form-of-001
-  var padding =
-    this.nonceIncr < 10 ? '000' :
-      this.nonceIncr < 100 ? '00' :
-        this.nonceIncr < 1000 ? '0' : '';
-  return now + padding + this.nonceIncr;
 }
 
 Bitpanda.prototype._get = function (market, action, callback, args) {
@@ -128,20 +108,25 @@ Bitpanda.prototype._get = function (market, action, callback, args) {
   this._request('get', path, undefined, callback, args)
 }
 
+Bitpanda.prototype._delete = function (market, action, callback, args) {
+  args = _.compactObject(args);
+
+  if (market) {
+    var path = '/public/v1/' + action + '/' + market;
+  }
+  else {
+    var path = '/public/v1/' + action;
+  }
+  path += (querystring.stringify(args, null, null, { encodeURIComponent: querystring.unescape }) === '' ? '' : '?') + querystring.stringify(args, null, null, { encodeURIComponent: querystring.unescape });
+  this._request('delete', path, undefined, callback, args)
+}
 
 Bitpanda.prototype._post = function (market, action, callback, args) {
-  if (!this.key)
-    return callback(new Error('Need Auth Key for API'));
 
   if (market)
     var path = '/public/v1/' + action + '/' + market;
   else
     var path = '/public/v1/' + action;
-
-  // var nonce = this._generateNonce();
-  // var message = nonce + this.client_id + this.key;
-  // var signer = crypto.createHmac('sha256', new Buffer(this.secret, 'utf8'));
-  // var signature = signer.update(message).digest('hex').toUpperCase();
 
   // args = _.extend({
   //   key: this.key,
@@ -152,35 +137,175 @@ Bitpanda.prototype._post = function (market, action, callback, args) {
   // args = _.compactObject(args);
   // var data = querystring.stringify(args);
 
+  var data = JSON.stringify(args);
   this._request('post', path, data, callback, args);
 }
 
 //
 // Public API
 //
+/**
+ * returns the Time in UTC from the Server into the callback function. 
+ */
 Bitpanda.prototype.time = function (callback) {
+  this.private = false;
   this._get(null, 'time', callback);
 }
-
+/**
+ * returns the Public fee structure into the callback function. 
+ */
 Bitpanda.prototype.fees = function (callback) {
+  this.private = false;
   this._get(null, 'fees', callback);
 }
-
+/**
+ * returns the list of available currencies into the callback function. 
+ */
 Bitpanda.prototype.currencies = function (callback) {
+  this.private = false;
   this._get(null, 'currencies', callback);
 }
-
+/**
+ * returns the list of available trade instruments into the callback function. 
+ */
 Bitpanda.prototype.instruments = function (callback) {
+  this.private = false;
   this._get(null, 'instruments', callback);
 }
-
+/**
+ * returns the order book for a given instrument(market) and compression level into the callback function. 
+ */
 Bitpanda.prototype.oderBook = function (market, level, callback) {
+  this.private = false;
   options = level;
   this._get(market, 'order-book', callback, options);
 }
-
+/**
+ * returns the candlesticks for a given instrument(market) for a closed time period into the callback function. 
+ */
 Bitpanda.prototype.candlesticks = function (market, options, callback) {
+  this.private = false;
   this._get(market, 'candlesticks', callback, options);
 }
 
+//
+// Privat API
+//
+
+/**
+ * returns the balance details for an account into the callback function. 
+ */
+Bitpanda.prototype.getBalances = function (callback) {
+  this.private = true;
+  if (!this.key)
+    return callback(new Error('Must provide key to make this API request.'));
+  else
+    this._get(null, 'account/balances', callback);
+}
+/**
+ * returns the fee details for an account into the callback function. 
+ */
+Bitpanda.prototype.getAccountFees = function (callback) {
+  this.private = true;
+  if (!this.key)
+    return callback(new Error('Must provide key to make this API request.'));
+  else
+    this._get(null, 'account/fees', callback);
+}
+/**
+ * returns the Orders for an account into the callback function. 
+ */
+Bitpanda.prototype.getOrders = function (market, options, callback) {
+  this.private = true;
+  if (!this.key)
+    return callback(new Error('Must provide key to make this API request.'));
+  else
+    if (market)
+      options['instrument_code'] = market;
+
+  this._get(null, 'account/orders', callback, options);
+}
+/**
+ * returns the Order information defined by an orderId for an account into the callback function. 
+ */
+Bitpanda.prototype.getOrderByID = function (id, callback) {
+  this.private = true;
+  if (!this.key)
+    return callback(new Error('Must provide key to make this API request.'));
+
+  this._get(id, 'account/orders', callback);
+}
+/**
+ * returns the trade information for a specific orderId into the callback function. 
+ */
+Bitpanda.prototype.getTradesByOrderID = function (id, callback) {
+  this.private = true;
+  if (!this.key)
+    return callback(new Error('Must provide key to make this API request.'));
+
+  this._get(id + '/trades', 'account/orders', callback);
+}
+/**
+ * returns a paginated report on past trades, sorted by timestamp (newest first) into the callback function. 
+ */
+Bitpanda.prototype.getTrades = function (options, callback) {
+  this.private = true;
+  if (!this.key)
+    return callback(new Error('Must provide key to make this API request.'));
+
+  this._get(null, 'account/trades', callback, options);
+}
+/**
+ * returns information for a trade into the callback function. 
+ */
+Bitpanda.prototype.getTradeByID = function (id, callback) {
+  this.private = true;
+  if (!this.key)
+    return callback(new Error('Must provide key to make this API request.'));
+
+  this._get(id, 'account/trades', callback);
+}
+
+/**
+ * returns the running Traidingvolume for this account into the callback function.
+ * It is calculated over a 30 day running window and updated once every 24hrs.
+ */
+Bitpanda.prototype.getTraidingVolume = function (callback) {
+  this.private = true;
+  if (!this.key)
+    return callback(new Error('Must provide key to make this API request.'));
+
+  this._get(null, 'account/trading-volume', callback);
+}
+/**
+ * Create a new order of the type LIMIT, MARKET or STOP. There is a minimum size per order which can be looked up by querying the /instruments endpoint. Additionally, the precision limitations can be found there.
+ */
+Bitpanda.prototype.postOrder = function (request_body, callback) {
+  this.private = true;
+  if (!this.key)
+    return callback(new Error('Must provide key to make this API request.'));
+
+  this._post(null, 'account/orders', callback, request_body);
+}
+/**
+ * Closes all Ordes or specify a Market to close all Orders from it.
+ */
+Bitpanda.prototype.deleteOrders = function (market, callback) {
+  options = {}
+  this.private = true;
+  if (!this.key)
+    return callback(new Error('Must provide key to make this API request.'));
+  options['instrument_code'] = market;
+  this._delete(null, 'account/orders', callback, options);
+}
+/**
+ * Closes the Order by the specified ID and returns {"order_deleted":true} into the callback function if succesfull deleted.
+ */
+Bitpanda.prototype.deleteOrderByID = function (id, callback) {
+  this.private = true;
+  if (!this.key)
+    return callback(new Error('Must provide key to make this API request.'));
+
+  this._delete(id, 'account/orders', callback);
+}
 module.exports = Bitpanda;
--- example.js
+++ example.js
@@ -1,19 +1,33 @@
 var Bitpanda = require('./bitpanda.js');
-
 var publicBitpanda = new Bitpanda();
 
 
-// publicBitpanda.time(console.log)
+publicBitpanda.time(console.log)
 // publicBitpanda.fees(console.log)
 // publicBitpanda.currencies(console.log)
 // publicBitpanda.instruments(console.log)
 // publicBitpanda.oderBook('BTC_EUR', { level: '1' }, console.log)
 // publicBitpanda.candlesticks('BTC_EUR', { unit: 'HOURS', period: '1', from: '2019-08-07T00:00:00.080Z', to: '2019-08-08T00:00:00.090Z' }, console.log)
 
+var key = 'your-API-KEY';
+var timeout = 10000;
+var host = 'api.exchange.bitpanda.com';
+var privateBitpanda = new Bitpanda(key, timeout, host);
 
-// var key = 'your-key';
-// var timeout = 10000;
-// var host = 'api.exchange.bitpanda.com';
-// var privateBitpanda = new Bitpanda(key, timeout, host);
+//ALL COMMENTED OUT FOR SAVETY RESONS
 
-
+// privateBitpanda.getBalances(console.log)
+// privateBitpanda.getAccountFees(console.log)
+// privateBitpanda.getOrders('BTC_EUR', {}, console.log) // Only open BTC_EUR Orders
+// privateBitpanda.getOrders(null, {}, console.log) // ALL open Orders
+// privateBitpanda.getOrders('BTC_EUR', { from: '2019-08-20T00:00:00.080Z', to: '2019-08-25T00:00:00.090Z', with_cancelled_and_rejected: 'true', with_just_filled_inactive: 'true', max_page_size: '100' }, console.log) // All Options example
+// privateBitpanda.getOrderByID('a23c649b-83cb-4cdf-ad85-e0ce81d5b08e', console.log) // Returns one OrderBy his OrderId
+// privateBitpanda.getTradesByOrderID('5b7fec58-f0c4-4440-a256-8f289ffb6799', console.log) //Returns trades of the OrderId
+// privateBitpanda.getTrades({}, console.log) //Last 100 Trades
+// privateBitpanda.getTrades({ from: '2019-08-10T00:00:00.080Z', to: '2019-08-25T00:00:00.090Z' }, console.log) //Trades in Time period
+// privateBitpanda.getTradeByID('0ee4a592-f9bb-4e29-bc63-fac14d475c06', console.log) //retuns the Traid Info for the TradeId
+// privateBitpanda.getTraidingVolume(console.log)//Get current Traiding Volume
+// privateBitpanda.postOrder({ instrument_code: 'BTC_EUR', side: 'BUY', type: 'LIMIT', amount: '11', price: '1.00' }, console.log) //creates a new Order acording to the request Body
+// privateBitpanda.deleteOrders('ETH_EUR', console.log) //closes all ordes in ETH_EUR Market
+// privateBitpanda.deleteOrders(null, console.log) //Closes all 
+// privateBitpanda.deleteOrderByID('d03d37b5-c0b8-4656-bb8b-192ca928e290', console.log) //closes the Order with the specified ID
